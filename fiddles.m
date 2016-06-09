f8777dad9a7900a3a74110fc7146dacaa076150c
CelsiaAdonis <spyraj@gmail.com>
2016-06-08 21:01:02 -0400

    full file

--- fiddles.m
+++ fiddles.m
@@ -1,8 +1,4 @@
-poop = {'A' 1; 'B' 2; 'C' 3};
-justice = ['1', 'a'; '4', '5'; '7', '2'];
+function fiddles(X, Y, varargin)
 
-snake = rand(4,5);
-goron = eye(10,10);
-
-foof = [1 1 4; 2 5 7;1 2 2];
-bibo = [1 1 1;2 2 2;3 3 3];
+usernote = input('Input note name: ', 's');
+note = upper(usernote);
